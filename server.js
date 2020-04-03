928681ec92698e99a63ba8ce2c3d0c443425923a
Pablo Chimiski <pchimiski@baufest.com>
2020-04-02 21:37:31 -0300

    otro fix

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-
-app.use(express.env.PORT || 8080);
-
+const paht = require('path');
+app.use(express.static(__dirname + '/angularapp'));
+app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
-
-console.log('build successful!!');
+console.log('Finalizo!! todo okkk');
